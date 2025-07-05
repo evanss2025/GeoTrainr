7aac03582a748871ecc02bd09247d1e84b347cef
Sophia E <125570727+evanss2025@users.noreply.github.com>
2025-07-04 23:59:34 -0400

    update

--- backend/yolov5/map.py
+++ backend/yolov5/map.py
@@ -79,7 +79,7 @@ def model(country):
         print(f"✅ downloaded {len(image_paths)} images")
 
 
-    weights_path = 'yolov5/best.pt'
+    weights_path = 'yolov5/runs/train/bollard_detector14/weights/best.pt'
     device = select_device('') 
     model = DetectMultiBackend(weights_path, device=device)
     model.eval()
